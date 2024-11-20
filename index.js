0fb9d03a8b9117321040f965952a0d42d4d6d5b9
Виталий <vitas2505@icloud.com>
2024-11-20 12:46:33 +0300

    Task 1

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 class Validator {
   email() {
-    return {
+    const emailValidator = {
+      minLength: null,
+      maxLength: null,
+
+      setEmailLengthConstraint(minLength, maxLength = null) {
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+        return this;
+      },
+
       isValid(value) {
-        return typeof value === 'string' && value.includes('@');
+        if (typeof value !== 'string' || !value.includes('@')) {
+          return false;
+        }
+
+        const localPart = value.split('@')[0];
+
+        if (this.minLength !== null && localPart.length < this.minLength) {
+          return false;
+        }
+
+        if (this.maxLength !== null && localPart.length > this.maxLength) {
+          return false;
+        }
+
+        return true;
       },
     };
+
+    return emailValidator;
+  }
+
+  age() {
+    const ageValidator = {
+      AAA: false,
+      isAdult() {
+        this.AAA = true;
+        return this;
+      },
+
+      isValid(value) {
+        if (typeof value !== 'number' || Number.isNaN(value)) {
+          return false;
+        }
+
+        if (value < 0) {
+          return false;
+        }
+
+        if (this.AAA && value < 18) {
+          return false;
+        }
+
+        return true;
+      },
+    };
+
+    return ageValidator;
   }
 }
 
