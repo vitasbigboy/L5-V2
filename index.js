a9ee3dbfc2f7af2941e752d4b0d518e28f23f4b5
Виталий <vitas2505@icloud.com>
2024-11-20 11:30:42 +0300

    task 1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class Validator {
+  email() {
+    return {
+      isValid(value) {
+        return typeof value === 'string' && value.includes('@');
+      },
+    };
+  }
+}
 
+export default Validator;
